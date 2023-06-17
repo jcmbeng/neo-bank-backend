c3006c7b90681b11c52777b59dfae6447836bff2
jean-claude.mbeng <jean-claude.mbeng@adservio.fr>
2023-06-17 13:35:36 +0200

    Mise à jour du fichier NeoBankBackendApplication pour ajouter l'appel de Tomcat

--- src/main/java/fr/adservio/neobankbackend/NeoBankBackendApplication.java
+++ src/main/java/fr/adservio/neobankbackend/NeoBankBackendApplication.java
@@ -4,12 +4,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 @SpringBootApplication
 @EnableJpaAuditing
-public class NeoBankBackendApplication {
+public class NeoBankBackendApplication extends SpringBootServletInitializer {
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(NeoBankBackendApplication.class);
+    }
     public static void main(String[] args) {
         SpringApplication.run(NeoBankBackendApplication.class, args);
     }
 
 }
+
